270186117a3a3a8ed47eaa5eec5eb7b85669c9f7
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-03-30 22:16:02 +0100

    feat: seed db

--- frontend/db/seed.ts
+++ frontend/db/seed.ts
@@ -1,6 +1,37 @@
-import { db, } from "astro:db";
+import { db, Comment, Author, Likes, Post } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  await db.insert().values([{}]);
+  await db.insert(Author).values([
+    {
+      authorId: 1,
+      name: "Felix Kuhbier",
+      email: "felix.kuhbier@gmx.de",
+      password: "password",
+      salt: null,
+    },
+  ]);
+  await db.insert(Comment).values([
+    {
+      authorId: 1,
+      content: "Hello World",
+      commentId: 1,
+      publishedAt: null,
+    },
+  ]);
+
+  await db.insert(Likes).values([{ likeId: 1, amount: 20 }]);
+
+  await db.insert(Post).values([
+    {
+      postId: 1,
+      author: "Felix Kuhbier",
+      body: "My First Post",
+      likeId: 1,
+      title: "First Post",
+      status: "published",
+      publishedAt: null,
+      commentId: 1,
+    },
+  ]);
 }
