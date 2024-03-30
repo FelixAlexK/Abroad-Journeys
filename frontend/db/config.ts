740b3556c688ab475611507762418b3f1bb550b6
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-03-30 22:16:28 +0100

    feat: add password and salt and email column

--- frontend/db/config.ts
+++ frontend/db/config.ts
@@ -11,6 +11,9 @@ const Author = defineTable({
   columns: {
     authorId: column.number({ primaryKey: true }),
     name: column.text(),
+    email: column.text({ unique: true }),
+    password: column.text(),
+    salt: column.text({ optional: true }),
   },
 });
 
@@ -18,7 +21,7 @@ const Comment = defineTable({
   columns: {
     commentId: column.number({ primaryKey: true }),
     content: column.text(),
-    publishedAt: column.date({ default: NOW }),
+    publishedAt: column.date({ default: NOW, optional: true }),
     authorId: column.number({ references: () => Author.columns.authorId }),
   },
 });
@@ -28,14 +31,15 @@ const Post = defineTable({
     postId: column.number({ primaryKey: true }),
     body: column.text({ multiline: true }),
     status: column.text({ default: "draft" }),
-    publishedAt: column.date(),
+    publishedAt: column.date({ default: NOW, optional: true }),
     author: column.text(),
     title: column.text(),
     likeId: column.number({ references: () => Likes.columns.likeId }),
+    commentId: column.number({ references: () => Comment.columns.commentId }),
   },
 });
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: { Comment },
+  tables: { Comment, Author, Post, Likes },
 });
