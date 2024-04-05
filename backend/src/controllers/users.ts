7c1f6c77271eeeb86bae46418d6a14c08d976b9d
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-04-05 18:41:20 +0200

    feat: add check if user is Admin

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,11 +1,21 @@
 import express from "express";
-import { getUsers } from "../db";
+import { getUsers, getUserById } from "../model/userModel";
+import { HttpStatusCodes } from "../helpers/HttpHelper";
 
 export const getAllUsers = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
+    const { user_id } = req.body;
+
+    if (!user_id) {
+      return res.sendStatus(HttpStatusCodes["bad-request"]);
+    }
+    const user = await getUserById(user_id);
+    if (user?.role === "BASIC") {
+      return res.sendStatus(HttpStatusCodes["forbidden"]);
+    }
     const users = await getUsers();
 
     return res.status(200).json(users).end();
