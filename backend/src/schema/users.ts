6d6cfabe6c71ac6b43dca63926daa87aaef65765
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-04-05 18:42:18 +0200

    chore: change namign and data types in schema

--- backend/src/schema/users.ts
+++ backend/src/schema/users.ts
@@ -7,9 +7,10 @@ import {
   pgEnum,
   uniqueIndex,
   unique,
+  timestamp,
 } from "drizzle-orm/pg-core";
 
-export const UserRole = pgEnum("userRole", ["ADMIN", "BASIC"]);
+export const UserRole = pgEnum("user_role", ["ADMIN", "BASIC"]);
 
 export const UserTable = pgTable(
   "users",
@@ -20,23 +21,26 @@ export const UserTable = pgTable(
     profile_picture: text("profile_picture"),
     first_name: text("first_name").notNull(),
     last_name: text("last_name").notNull(),
-    created_at: date("created_at", { mode: "string" }),
-    updated_at: date("updated_at", { mode: "string" }),
-    last_login: date("last_login", { mode: "string" }),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updated_at").defaultNow().notNull(),
+    last_login: timestamp("last_login").defaultNow().notNull(),
     is_active: boolean("is_active"),
     password: text("password").notNull(),
     salt: text("salt"),
-    session_token: text("sessionToken"),
-    role: UserRole("userRole").default("BASIC").notNull(),
+    session_token: text("session_token"),
+    role: UserRole("user_role").default("BASIC").notNull(),
   },
   (users) => {
     return {
-      usernameIdx: uniqueIndex("usernameIdx").on(users.username),
-      emailIdx: uniqueIndex("emailIdx").on(users.email),
-      uniqueUsernameAndEmail: unique("uniqueUsernameAndEmail").on(
+      usernameIdx: uniqueIndex("username_idx").on(users.username),
+      emailIdx: uniqueIndex("email_idx").on(users.email),
+      uniqueUsernameAndEmail: unique("unique_username_and_email").on(
         users.username,
         users.email
       ),
     };
   }
 );
+
+export type User = typeof UserTable.$inferSelect;
+export type NewUser = typeof UserTable.$inferInsert;
