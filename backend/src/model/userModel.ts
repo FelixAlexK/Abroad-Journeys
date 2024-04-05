889cd4fa01032dd13e8eb7618e564817a3f610d7
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-04-05 18:39:48 +0200

    feat: add model for user

--- backend/src/model/userModel.ts
+++ backend/src/model/userModel.ts
@@ -1,25 +1,56 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { db } from "../db";
-import { UserTable } from "../schema/users";
+import { UserTable, NewUser, User } from "../schema/users";
+import { eq } from "drizzle-orm";
+
+//Create
+
+export const createUser = async (new_user: NewUser) => {
+  return await db.insert(UserTable).values(new_user).returning({
+    user_id: UserTable.user_id,
+    username: UserTable.username,
+    email: UserTable.email,
+    first_name: UserTable.first_name,
+    last_name: UserTable.last_name,
+  });
+};
+
+// Read
 
 export const getUserByEmail = async (email: string) => {
   return await db.query.UserTable.findFirst({
-    columns: {},
-    with: { posts: true },
+    where: (table, funcs) => funcs.eq(table.email, email),
   });
 };
 
-export const getPost = async () => {
-  await db.insert(UserTable).values({
-    email: "felix@email.com",
-    username: "Felix",
-    first_name: "Felix",
-    last_name: "Kuhbier",
-    password: "password",
-    role: "BASIC",
-    
-  });
-  //return await db.query.PostTable.findMany({ with: { author: true } });
+export const getUsers = async () => {
+  return await db.query.UserTable.findMany();
 };
 
-getPost();
+export const getUserBySessionToken = async (session_token: string) => {
+  return await db.query.UserTable.findFirst({
+    where: (table, funcs) => funcs.eq(table.session_token, session_token),
+  });
+};
+export const getUserById = async (user_id: string) => {
+  return await db.query.UserTable.findFirst({
+    where: (table, funcs) => funcs.eq(table.user_id, user_id),
+  });
+};
+
+// Update
+
+export const updateUserById = async (user_id: string, updated_user: User) => {
+  return await db
+    .update(UserTable)
+    .set(updated_user)
+    .where(eq(UserTable.user_id, user_id))
+    .returning({
+      user_id: UserTable.user_id,
+      username: UserTable.username,
+      email: UserTable.email,
+      profile_picture: UserTable.profile_picture,
+      first_name: UserTable.first_name,
+      last_name: UserTable.last_name,
+    });
+};
