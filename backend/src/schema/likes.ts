b626320464d79e1515dfa008dfcd70c4170c822e
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-04-12 17:53:41 +0200

    feat: export types

--- backend/src/schema/likes.ts
+++ backend/src/schema/likes.ts
@@ -1,19 +1,15 @@
-import {
-    pgTable,
-    uuid,
-    integer,
-  } from "drizzle-orm/pg-core";
-  
-  import { PostTable } from "./posts";
-  
-  export const LikeTable = pgTable("likes", {
-    like_id: uuid("like_id").primaryKey().defaultRandom(),
-    amount: integer("amount").default(0).notNull(),
-  
-    post_id: uuid("post_id")
-      .references(() => PostTable.post_id)
-      .notNull(),
-  
-    
-  });
-  
+import { pgTable, uuid, integer } from "drizzle-orm/pg-core";
+
+import { PostTable } from "./posts";
+
+export const LikeTable = pgTable("likes", {
+  like_id: uuid("like_id").primaryKey().defaultRandom(),
+  amount: integer("amount").default(0).notNull(),
+
+  post_id: uuid("post_id")
+    .references(() => PostTable.post_id)
+    .notNull(),
+});
+
+export type Like = typeof LikeTable.$inferSelect;
+export type NewLike = typeof LikeTable.$inferInsert;
